b3fd7df82335c4b5fdc5b5a996ec3b303da792af
robertc <robert.campbell.qslr@gmail.com>
2023-01-31 18:08:14 -0600

    close-smoke-test-branch

--- api/handlers/stack_handler.go
+++ api/handlers/stack_handler.go
@@ -3,18 +3,8 @@ package handlers
 import "github.com/gofiber/fiber/v2"
 
 
+// smoke test Hello endpoint
 func GetStack(c *fiber.Ctx)  error {
 	return c.SendString("Hello, World 👋!")
 }
 
-
-// func GetStack(service stack.Service) fiber.Handler {
-// 	return func(c *fiber.Ctx) error {
-// 		fetched, err := service.Get
-// 		if err != nil {
-// 			c.Status(http.StatusInternalServerError)
-// 			return c.JSON(presenter.BookErrorResponse(err))
-// 		}
-// 		return c.JSON(presenter.BooksSuccessResponse(fetched))
-// 	}
-// }
